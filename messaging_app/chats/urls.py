0c58cd38d50644edf27b0beb4a997fbc58d40753
Judith Ogbole <judithogbole71@gmail.com>
2025-11-22 15:46:18 +0100

    urls added

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -7,4 +7,10 @@ router.register(r'users', UserViewSet, basename='user')
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
+messages_router = routers.NestedDefaultRouter(router, r'messages', lookup='message')
+messages_router.register(r'comments', MessageViewSet, basename='message-comments')
+
+convo_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+convo_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
 urlpatterns = [] + router.urls
