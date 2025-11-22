ab248c9aef5a16e1735b02909be77fe9348d9ce2
Judith Ogbole <judithogbole71@gmail.com>
2025-11-22 13:58:55 +0100

    Settings correction

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -125,6 +125,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
